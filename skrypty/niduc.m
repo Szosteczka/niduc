17e3ceb9d9dffa0c060ea37f3d675ef7a05923a3
Szosteczka <michal@bujak.org.pl>
2015-04-13 16:57:39 +0200

    [FIX] niduc.m używa klasy BinSig

--- skrypty/niduc.m
+++ skrypty/niduc.m
@@ -1,8 +1,4 @@
 %N jest liczb¹ generowanych bitów
-N = input('Podaj liczbê bitów do wygenerowania');
-PID = fopen('signal.txt', 'w');
+N = input('Podaj liczbê bitów do wygenerowania\n');
 
-x = randi(2,N,1) - 1; %randi generuje losowe wartoœci typu int miêdzy 1 i 2, gdy od wyniku odejmiemy '1' otrzymamy losowy ci¹g zer i jedynek
-
-fprintf(PID, '%i\n',x);
-fclose(PID);
+BinSig(N, 'sygnal.txt');
