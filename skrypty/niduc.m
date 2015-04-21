3bdc36d6e2bfc85e30dbd5e2fdb5b38a72dfc75b
Szosteczka <michal@bujak.org.pl>
2015-04-22 00:36:28 +0200

    [FEATURE] Klasa modulatora PSK+zastosowanie w niduc.m

--- skrypty/niduc.m
+++ skrypty/niduc.m
@@ -1,4 +1,8 @@
 %N jest liczb¹ generowanych bitów
 N = input('Podaj liczbê bitów do wygenerowania\n');
 
-BinSig(N, 'sygnal.txt');
+Syg = BinSig(N, 'sygnal.txt');
+PlotBinSig(Syg, 2,1,1);
+
+ModSyg = PSKMod('sygnal.txt',1,3,100,'SygnalZmodulowany.txt');
+PlotPSKMod(ModSyg, 2,1,2);
