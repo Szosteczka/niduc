d9f95e9c45bce4642b7f8cb7711cd7f2cea822e7
mdrygielski <drygielski.marcin@gmail.com>
2015-06-16 20:09:39 +0200

    no message

--- skrypty/NPSK.m
+++ skrypty/NPSK.m
@@ -1,34 +1,187 @@
 classdef NPSK
+    
     properties
+        N_m = 0;
         variant_m = 2;
         signal_m = 0;
+        amplitude_m = 1;
+        sampleRate_m = 10;
         ampNoise_m = 0;
         phNoise_m = 0;
         filename_m = 0;
         modulatedSignal_m = 0;
+        transmittedSignal_m = 0;
+        demodulatedSignal_m = 0;
+        Time_m = 0;
+        fileID_m = 0;
+        txpsk = 0;
+        rxpsk = 0;
+        recovpsk = 0;
+        BER = 0;
     end
+    
     methods
-        function obj = NPSK(filename, variant, ampNoise, phNoise)
-            
-            obj.filename_m = filename;
+        function obj = NPSK(variant, amplitude, sampleRate)
             obj.variant_m = variant;
+            obj.amplitude_m = amplitude;
+            obj.sampleRate_m = sampleRate;
+        end
+        
+        function obj =  Generate(obj, elements, filename)
+            obj.N_m = elements; %liczba elementów ustawiana przez argument
+            if ~strcmp(filename,'')
+                obj.filename_m = filename;
+                obj.fileID_m = fopen(obj.filename_m, 'w');
+            end
+            obj.signal_m = randi([0 obj.variant_m-1],obj.N_m,1);
+            %powy¿sza funkcja randi generuje losowy int z przedzia³u
+            %tu przedzia³ [1,2], i wype³nia nimi odpowiedni¹ macierz.
+            %Poniewa¿ chcê sygna³ binarny, od wygenerowanych cyfr 1 i 2
+            %odejmujê ostatecznie 1.
+            if ~strcmp(filename,'')
+                fprintf(obj.fileID_m, '%i\n',obj.signal_m);%zapis do pliku
+                fclose(obj.fileID_m);
+            end
+        end
+        
+        function PlotBitSignal(obj,m,n,p)
+            
+            y=obj.signal_m;
+            y=y(:);
+            figure;
+            y=[y';y'];
+            y=y(:);
+            x=1:numel(y)/2;
+            x=[x;x];
+            x=x(:);
+            x(1)=[];
+            y(end)=[];
+            subplot(m,n,p);
+            stem(y);
+            xlim([x(1) x(end)]);
+            title('Sygna³ binarny');
+        end
+        
+        
+        
+        function obj = Modulate(obj, filename, out_filename)
+            if ~strcmp(filename,'')
+                obj.filename_m = filename;
+                fid = fopen( filename, 'r');
+                data = fscanf(fid, '%i');
+            end
+            data = obj.signal_m;
+            
+            t = 1/obj.sampleRate_m: 1/obj.sampleRate_m : 1;
+            
+            obj.txpsk = pskmod(data,obj.variant_m);
+            for ii = 1: 1: length(obj.txpsk)
+                t =  t + 1;
+                obj.modulatedSignal_m = [obj.modulatedSignal_m obj.amplitude_m * cos(2*pi * t + angle(obj.txpsk(ii)))];
+                obj.Time_m = [obj.Time_m t];
+            end
+            
+            if ~strcmp(out_filename,'')
+                SaveFileID = fopen(out_filename, 'w');
+                fprintf(SaveFileID, '%i\n',obj.modulatedSignal_m);%zapis do pliku
+                fclose(SaveFileID);
+            end
+            
+        end
+        
+        
+        function PlotNPSKModulator(obj, m, n, p) 
+            
+            subplot(m,n,p);
+            plot(obj.Time_m,obj.modulatedSignal_m,'LineWidth',2);
+            
+            xlabel('Czas');
+            ylabel('Amplituda');
+            title('Sygna³ zmodulowany');
+            
+            axis([1 obj.Time_m(end)-1 -1.5 1.5]);
+            grid  on;
+        end
+        
+        
+         function obj = TransmissionChannel(obj, filename, ampNoise, phNoise)
             obj.ampNoise_m = ampNoise;
             obj.phNoise_m = phNoise;
-            
-            fid = fopen(filename, 'r');
-            obj.signal_m = fscanf(fid, '%i');
+             
+             
+            phasenoise = randn(length(obj.signal_m),1) * obj.phNoise_m;
+            amplitudenoise =  randn(length(obj.signal_m),1) * obj.ampNoise_m;
+            obj.rxpsk = obj.txpsk .* exp(2i*pi*phasenoise);
+            obj.rxpsk = obj.rxpsk + amplitudenoise;
 
+            t = 1/obj.sampleRate_m: 1/obj.sampleRate_m : 1;
+            for ii = 1: 1: length(obj.rxpsk)
+                t =  t + 1;
+                obj.transmittedSignal_m = [obj.transmittedSignal_m cos(2*pi * t + angle(obj.rxpsk(ii))) .* exp(2i*pi*phasenoise(ii)) + amplitudenoise(ii)];
+            end
             
-            data = obj.signal_m;
-            hModulator = comm.PSKModulator(obj.variant_m, 'BitInput',true);
-            hModulator.PhaseOffset = pi/4;
-            modData = step(hModulator, data);
-            
-            hAWGN = comm.AWGNChannel('EbNo',ampNoise,'BitsPerSymbol',3);
-            
-            channelOutput = step(hAWGN,modData);
-            scatterplot(channelOutput)
-
+            if ~strcmp(filename,'')
+                SaveFileID = fopen(filename, 'w');
+                fprintf(SaveFileID, '%i\n',obj.transmittedSignal_m);%zapis do pliku
+                fclose(SaveFileID);
+            end
         end
+        %Funkcja rysuj¹ca sygna³ zmodulowany
+        function PlotTransmissionChannel(obj, m, n, p) 
+            subplot(m,n,p);
+            plot(obj.Time_m,obj.transmittedSignal_m,'LineWidth',2);
+            
+            xlabel('Czas');
+            ylabel('Amplituda');
+            title('Sygna³ Zniekszta³cony');
+            
+            axis([1 obj.Time_m(end)-1 -1.5 1.5]);
+            grid  on;
+            
+        end
+        
+        
+        function obj = Demodulate(obj, filename)
+            obj.recovpsk = pskdemod(obj.rxpsk,obj.variant_m);
+            
+            numerrs_psk = symerr(obj.signal_m,obj.recovpsk);
+            obj.BER = numerrs_psk/length(obj.signal_m);
+            if ~strcmp(filename,'')
+                SaveFileID = fopen(filename, 'w');
+                fprintf(SaveFileID, '%i\n',obj.transmittedSignal_m);%zapis do pliku
+                fclose(SaveFileID);
+            end
+            
+        end
+        
+        function PlotNPSKDemodulator(obj, m, n, p) 
+            
+            y=obj.recovpsk;
+            y=y(:);
+            y=[y';y'];
+            y=y(:);
+            x=1:numel(y)/2;
+            x=[x;x];
+            x=x(:);
+            x(1)=[];
+            y(end)=[];
+            subplot(m,n,p);
+            stem(y);
+            xlim([x(1) x(end)]);
+            title('Sygna³ binarny odebrany');
+        end
+       
+        function PlotNPSKnoise(obj) 
+            scatterplot(obj.rxpsk);
+            title('Sygna³ binarny odebrany');
+        end
+        
+        function showBER(obj) 
+            
+            BER = obj.BER
+        end
+        
     end
+    
 end
+
--- skrypty/niduc.m
+++ skrypty/niduc.m
@@ -1,41 +1,36 @@
-%N jest liczb¹ generowanych bitów
-N = input('Podaj liczbê bitów do wygenerowania\n');
+clc;
+clear all;
 
-Syg = BinSig(N, 'sygnal.txt');
-PlotBinSig(Syg, 4,1,1);
-clear classes;
-
-ModSyg = PSKMod('sygnal.txt',1,3,100,'SygnalZmodulowany.txt');
-PlotPSKMod(ModSyg, 4,1,2);
-clear classes;
-
-NoiseSyg = TransmissionChannel('SygnalZmodulowany.txt',100,'SygnalZniekszatalcony.txt',30);
-PlotTransmissionChannel(NoiseSyg, 4,1,3);
+Variant = 8;
+Amplitude = 1;
+SampleRate = 50;
+N = 256;
+ampNoise = .05;
+phNoise = .01;
 
 
-DeModSyg = PSKDeMod('SygnalZniekszatalcony.txt',100,'SygnalOdkodowany.txt');
-PlotPSKDeMod(DeModSyg, 4,1,4)
-PlotPSKDeMod1(DeModSyg);
+obj = NPSK(Variant,Amplitude,SampleRate);
+
+obj = Generate(obj,N,'sygnal.txt');
+
+PlotBitSignal(obj,4,1,1);
+
+obj = Modulate(obj, 'sygnal.txt', 'sygnalZmodulowany.txt');
+
+PlotNPSKModulator(obj, 4, 1, 2);
+
+obj = TransmissionChannel(obj, 'SygnalZnieksztalcony.txt', ampNoise, phNoise);
 
 
-DataFileID1 = fopen('sygnal.txt', 'r');
-SignalData1 = fscanf(DataFileID1, '%i');
-DataFileID2 = fopen('SygnalOdkodowany.txt', 'r');
-SignalData2 = fscanf(DataFileID2, '%i');
-Errors = 0;
+PlotTransmissionChannel(obj, 4, 1, 3);
 
-for j=1:length(SignalData1)
-    if(SignalData1(j) ~= SignalData2(j))
-        Errors = Errors + 1;
-    end  
-end
 
-BER = Errors/length(SignalData1);
+obj = Demodulate(obj, 'sygnalOdebrany.txt');
 
-display(BER);
+PlotNPSKDemodulator(obj, 4, 1, 4);
 
-QPSKSignal = NPSK('sygnal.txt', 4, 30, 30);
+PlotNPSKnoise(obj);
 
-QPSKSignalNoisy = NPSK('sygnal.txt', 4, 15, 15);
+showBER(obj);
 
-OPSKSignal = NPSK('sygnal.txt', 8, 30, 30);
+
